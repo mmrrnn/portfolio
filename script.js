3d961f9cbda33f091ae3eedb15a72c5800f99fc9
Marcin Papież <mmpapiez@gmail.com>
2020-08-27 18:30:27 +0200

    Added animations

--- script.js
+++ script.js
@@ -10,3 +10,60 @@ const ageCalculator = () => {
 }
 
 ageContainer.innerText = ageCalculator();
+
+//// SCROLL REVEAL
+
+ScrollReveal().reveal('.main-container h1', {
+    distance: '150%',
+    origin: 'bottom',
+    opacity: null,
+    duration: 800
+});
+
+ScrollReveal().reveal('.main-container h3', {
+    distance: '150%',
+    origin: 'bottom',
+    duration: 1500
+});
+
+ScrollReveal().reveal('.container-header', {
+    distance: '150%',
+    origin: 'bottom',
+    duration: 1000
+});
+
+ScrollReveal().reveal('.icon', {
+    distance: '100%',
+    origin: 'top',
+    duration: 1000
+});
+
+ScrollReveal().reveal('.icon-card h4', {
+    distance: '150%',
+    origin: 'bottom',
+    duration: 800
+});
+
+ScrollReveal().reveal('.icon-card p', {
+    distance: '50%',
+    origin: 'bottom',
+    duration: 1200
+});
+
+ScrollReveal().reveal('.card-body', {
+    distance: '50%',
+    origin: 'top',
+    duration: 800
+});
+
+ScrollReveal().reveal('.why-me-container', {
+    distance: '50%',
+    origin: 'top',
+    duration: 800
+});
+
+ScrollReveal().reveal('footer .col-12', {
+    distance: '150%',
+    origin: 'top',
+    duration: 800
+});
